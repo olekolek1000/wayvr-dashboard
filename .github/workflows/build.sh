ad988f85b9cac247fda40bcb4af290d96cd558b9
Aleksander <aleksander@oo8.dev>
2025-05-06 18:32:24 +0200

    workflows: Add PACKAGE_APPIMAGE env

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -15,7 +15,6 @@ echo "REPO_DIR: ${REPO_DIR}"
 echo "TEMP_DIR: ${TEMP_DIR}"
 echo "APP_DIR: ${APP_DIR}"
 
-
 # Run "configure_env.sh" only once by creating an empty ".installed" file.
 # This will always run on a github worker
 if ! test -f ${TEMP_DIR}/.installed; then
@@ -29,14 +28,17 @@ if ! test -f ${TEMP_DIR}/.installed; then
 fi
 
 ${SCRIPT_DIR}/compile.sh
-${SCRIPT_DIR}/appimage.sh
 
-ARTIFACTS="${REPO_DIR}/artifacts"
-echo "Copying artifacts to ${ARTIFACTS}"
+if [ -n "$PACKAGE_APPIMAGE" ]; then
+	${SCRIPT_DIR}/appimage.sh
 
-mkdir -p ${ARTIFACTS}
+	ARTIFACTS="${REPO_DIR}/artifacts"
+	echo "Copying artifacts to ${ARTIFACTS}"
 
-cp "${TEMP_DIR}/wayvr-dashboard" "${ARTIFACTS}/"
-cp "${TEMP_DIR}/WayVR_Dashboard-x86_64.AppImage" "${ARTIFACTS}/wayvr-dashboard.AppImage"
+	mkdir -p ${ARTIFACTS}
+
+	cp "${TEMP_DIR}/wayvr-dashboard" "${ARTIFACTS}/"
+	cp "${TEMP_DIR}/WayVR_Dashboard-x86_64.AppImage" "${ARTIFACTS}/wayvr-dashboard.AppImage"
+fi
 
 echo "Build done"
